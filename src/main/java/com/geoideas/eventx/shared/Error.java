a30ad8fa9e7f54d1d059ae7089edec9d4f5d1430
Owen Grant <owen.dalmon.grant@gmail.com>
2019-07-29 07:32:56 -0400

    added error codes of unauthorised

--- src/main/java/com/geoideas/eventx/shared/Error.java
+++ src/main/java/com/geoideas/eventx/shared/Error.java
@@ -10,11 +10,13 @@ package com.geoideas.eventx.shared;
  * @author owen
  */
 public class Error {
+    public static final int UNAUTHORISED_ERROR = 0000;
     public static final int EVENT_CONTENT_ERROR = 1000;
     public static final int DUPLICATE_EVENT = 1001;
     public static final int DUPLICATE_HASH = 1002;
     public static final int UNKNOWN_ERROR = 1111;
 
+    public static final String UNAUTHORISED_ERROR_MESSAGE = "Unauthorized";
     public static final String DUPICATE = "DUPLICATE";
     public static final String EVENT_CONTENT_ERROR_MESSAGE = "Error in the event message format";
     public static final String DUPLICATE_EVENT_MESSAGE = DUPICATE+": An event with the same CONTEXT, ENTITY, ENTITYID and REVISION already exists";
